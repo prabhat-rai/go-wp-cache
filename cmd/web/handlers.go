cffc49951ee14a9d39b65b27cd51a8355e40a17f
prabhat rai <prabhat.rai@landmarkgroup.in>
2021-03-29 02:22:59 +0530

    Return result as JSON response

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -10,5 +10,8 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 
 
 func (app *application) fetchWordpressData(w http.ResponseWriter, r *http.Request) {
-	fetchWordpressData("pkrai.wordpress.com", "71978482")
+	response := fetchWordpressData("pkrai.wordpress.com", "71978482")
+	finalResponse := "{\"posts\" : " + response[0] + ", \"categories\" : " + response[1] + ", \"tags\" : " + response[2] +"}"
+	w.Header().Set("Content-Type", "application/json")
+	w.Write([]byte(finalResponse))
 }
